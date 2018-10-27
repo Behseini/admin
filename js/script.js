f90b78d64a13d25548f88c47efb2342612e82d05
behseini <behseini@hotmail.com>
2018-10-26 18:11:35 -0700

    nam

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $("#toggler").on("click", function(){
 $('[data-toggle="popover"]').popover({
         placement : 'right',
         html : true,
-        title : 'User Info <a href="#" class="close" data-dismiss="alert">&times;</a>',
+        title : 'Assets <a href="#" class="close" data-dismiss="alert">&times;</a>',
         content : '<div class="media"><a href="#" class="pull-left"><img src="/examples/images/avatar-tiny.jpg" class="media-object" alt="Sample Image"></a><div class="media-body"><h4 class="media-heading">Jhon Carter</h4><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
     });
     $(document).on("click", ".popover .close" , function(){
