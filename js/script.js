751caf4f6f23c35e0d5b78e4097fb27aeb61c789
Behrouz <behseini@hotmail>
2018-10-29 07:33:22 -0700

    new

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
-$("#toggler").on("click", function(){
-   $("#sidebar").toggleClass('max-sidebar');
-    $("#sidebar table td").toggleClass('fadein-td fadeout-td');
-    //$("#sidebar table td:nth-child(2)").toggleClass('hidden-td');
-   $("#content").toggleClass('max-content');
-    $(".mar").toggleClass('d-none');
-    $(".mar1").toggleClass('d-none');
-});
+$("#handler").on("click", function(){
+    $('.collapse').collapse('hide');
+    $(this).find('.icon').toggleClass('icon-arrow-right icon-arrow-left');
+    $("#sidebar").toggleClass('max-sidebar');
+    $("#content").toggleClass('max-content');
+    setTimeout(function(){
+        $('.btn-card-n').toggleClass('d-none');
+        $('.btn-card-w').toggleClass('d-none');
+     }, 100);
 
-$('[data-toggle="popover"]').popover({
-        placement : 'right',
-        html : true,
-        title : 'Assets <a href="#" class="close" data-dismiss="alert">&times;</a>',
-        content : '<div class="media"><a href="#" class="pull-left"><img src="/examples/images/avatar-tiny.jpg" class="media-object" alt="Sample Image"></a><div class="media-body"><h4 class="media-heading">Jhon Carter</h4><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
-    });
-    $(document).on("click", ".popover .close" , function(){
-        $(this).parents(".popover").popover('hide');
-    });
+ });
