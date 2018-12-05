cc882ba3102de929f1d4b20112b2965516e9b837
behseini <behseini@hotmail.com>
2018-12-04 21:25:21 -0800

    nv

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 
 $("#close").on("click", function(){
-   $("#sidebar").css('right', '-25%');
-   $("#main").css('width', '100%');
+   $("#sidebar").toggleClass('sidebar-d-none sidebar-d-show');
+   $("#main").toggleClass('main-d-full main-d-side');
 });
 
 
-$(".open").on("click", function(){
-   $("#sidebar").css('right', '0');
-   $("#main").css('width', '75%');
+$(".full").on("click", function(){
+   $("#sidebar").removeClass('sidebar-d-none').addClass('sidebar-d-show');
+   $("#main").removeClass('main-d-full').addClass('main-d-side');
 });
+
+
+$('input').focus(function(){
+  $(this).parents('.form-group').addClass('focused');
+});
+
+$('input').blur(function(){
+  var inputValue = $(this).val();
+  if ( inputValue == "" ) {
+    $(this).removeClass('filled');
+    $(this).parents('.form-group').removeClass('focused');
+  } else {
+    $(this).addClass('filled');
+  }
+})
